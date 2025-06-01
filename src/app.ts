6e9c86500f551f8e78024880a44585c920b3172b
rafaelfrodz <contato.frodz@hotmail.com>
2025-06-01 19:53:59 -0300

    Iniciando da aplicacao criando o esqueleto do app

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({message: "Hello World!"});
+
+});
+
+app.post("/auth/login", (req, res) => {
+  const {email, password} = req.body;
+  console.log(email, password);
+  res.send()
+});
+
+app.post("/partners", (req, res) => {
+  const {name, email, password, company_name} = req.body;
+});
+
+app.post("/customers", (req, res) => {
+  const {name, email, password, adress, telefone} = req.body;
+
+});
+
+app.get("/events", (req, res) => {
+
+});
+
+app.get("/events/:eventId", (req, res) => {
+  const {eventId} = req.params;
+  console.log(eventId);
+  res.send()
+});
+
+app.post("/partners/events", (req, res) => {
+  const {name, description, date, location} = req.body;
+
+});
+
+app.get("/partners/events", (req, res) => {
+
+});
+
+app.get("/partners/events/:eventId", (req, res) => {
+  const {eventId} = req.params;
+  console.log(eventId);
+  res.send()
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
